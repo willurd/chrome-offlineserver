d0a3547f01bc23703db622581af81dae577d4a8b
William Bowers <william.bowers@gmail.com>
2014-08-21 19:44:30 -0700

    Added server-item

---
+++ js/directives/server-item.js
@@ -0,0 +1,15 @@
+var app = angular.module('app');
+
+app.directive('serverItem', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/js/directives/server-item.html',
+    scope: {
+      address: '=',
+      port: '='
+    },
+    controller: function($scope) {
+
+    }
+  }
+});
