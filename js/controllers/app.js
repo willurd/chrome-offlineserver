cac2d9aa52a62cd7268a9be94888090f9cf05af1
William Bowers <william.bowers@gmail.com>
2014-08-21 19:44:12 -0700

    Displaying multiple servers

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -1,7 +1,8 @@
 angular.module('app')
   .controller('AppController', function($scope) {
-    $scope.$watch('networkInterface', function() {
-      var i = $scope.networkInterface || {};
-      console.log('Interface changed:', i.name, i.address);
-    });
+    $scope.servers = [
+      { address: '127.0.0.1', port: 8001 },
+      { address: '2601:9:8280:1d0:1e3e:84ff:fe0e:5d1', port: 8002 },
+      { address: '10.0.0.16', port: 8003 },
+    ];
   });
--- js/directives/network-interface-selector.js
+++ js/directives/network-interface-selector.js
@@ -1,29 +1,44 @@
-angular.module('app')
-  .directive('networkInterfaceSelector', function($compile, $templateCache) {
-    var localHostInterface = {
-      address: '127.0.0.1',
-      name: 'localhost'
-    };
+var app = angular.module('app');
 
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: '/js/directives/network-interface-selector.html',
-      scope: {
-        value: "="
-      },
-      controller: function($scope) {
-        $scope.value = localHostInterface;
-        $scope.interfaces = [
-          localHostInterface
-        ];
+app.directive('networkInterfaceSelector', function() {
+  var localHostInterface = {
+    address: '127.0.0.1',
+    name: 'localhost'
+  };
 
-        // TODO: Abstract this out to a service.
-        chrome.system.network.getNetworkInterfaces(function(interfaces) {
-          $scope.$apply(function() {
-            $scope.interfaces = $scope.interfaces.concat(interfaces);
-          });
-        });
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: '/js/directives/network-interface-selector.html',
+    scope: {
+      value: "="
+    },
+    controller: function($scope) {
+      $scope.value = $scope.value || localHostInterface;
+      $scope.interfaces = [
+        localHostInterface
+      ];
+
+      function getInterfaceWithAddress(address) {
+        for (var i = 0, len = $scope.interfaces.length; i < len; i++) {
+          if ($scope.interfaces[i].address === address) {
+            return $scope.interfaces[i];
+          }
+        }
+
+        return null;
       }
-    };
-  });
+
+      // TODO: Abstract this out to a service.
+      chrome.system.network.getNetworkInterfaces(function(interfaces) {
+        $scope.$apply(function() {
+          $scope.interfaces = $scope.interfaces.concat(interfaces);
+
+          if (typeof $scope.value === 'string') {
+            $scope.value = getInterfaceWithAddress($scope.value) || localHostInterface;
+          }
+        });
+      });
+    }
+  };
+});
